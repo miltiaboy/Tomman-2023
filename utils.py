5df480e29e7466db9f849262aa4c9ac7a4a9bf1a
miltiaboy <114646328+miltiaboy@users.noreply.github.com>
2022-12-26 16:13:15 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -99,8 +99,8 @@ async def get_poster(query, bulk=False, id=False, file=None):
             plot = plot[0]
     else:
         plot = movie.get('plot outline')
-    if plot and len(plot) > 153:
-        plot = plot[0:153] + "..."
+    if plot and len(plot) > 135:
+        plot = plot[0:135] + "..."
 
     return {
         'title': movie.get('title'),
